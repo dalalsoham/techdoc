e22037acfcbe7e626d50871f54f9617db257f28a
mr_sd_jack_003 <barson0habra@gmail.com>
2024-09-08 01:47:18 +0530

    change some part

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -35,19 +35,19 @@ const FeatureList = [
   // },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
+// function Feature({Svg, title, description}) {
+//   return (
+//     <div className={clsx('col col--4')}>
+//       <div className="text--center">
+//         <Svg className={styles.featureSvg} role="img" />
+//       </div>
+//       <div className="text--center padding-horiz--md">
+//         <Heading as="h3">{title}</Heading>
+//         <p>{description}</p>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default function HomepageFeatures() {
   return (
